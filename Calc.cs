25399f5c650b01accaeb01b6d81493aa4092cc1c
Prog-Maker <salihovin@mail.ru>
2020-02-25 09:32:18 +0500

    Добавьте файлы проекта.

---
+++ Calc.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Calc
+{
+    //public class Calc<T> where T : IGeometricFigureCalculator
+    //{
+    //    Dictionary<string, IGeometricFigureCalculator> calculators = new Dictionary<string, IGeometricFigureCalculator>();
+
+    //    public void AddCalculator(IGeometricFigureCalculator calculator)
+    //    {
+    //        if (!calculators.ContainsKey(calculator.Name))
+    //        {
+    //            calculators.Add(calculator.Name, calculator);
+    //        }
+    //    }
+
+    //    public T CreateCalculator()
+    //    {
+    //        return Activator.CreateInstance<T>();
+    //    }
+
+    //}
+}
